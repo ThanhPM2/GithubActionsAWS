d8245fb1a93648ff317209fbfdc990ec732c5f04
ThanhPM <>
2022-08-25 15:51:40 +0700

    comment out WorkflowApplicationTest

--- src/test/java/com/workflow/github/WorkflowApplicationTest.java
+++ src/test/java/com/workflow/github/WorkflowApplicationTest.java
@@ -3,9 +3,9 @@ package com.workflow.github;
 import org.junit.jupiter.api.Test;
 
 class WorkflowApplicationTest {
-
-    @Test
-    public void main() {
-        WorkflowApplication.main(new String[]{});
-    }
+//
+//    @Test
+//    public void main() {
+//        WorkflowApplication.main(new String[]{});
+//    }
 }
