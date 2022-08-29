989ad1f7e65e7edbb1c39f0babb7a38bd06ea5ff
ThanhPM <>
2022-08-29 16:52:59 +0700

    deploy with docker and test hello 6

--- src/test/java/com/workflow/github/IntegrationEndpointTest.java
+++ src/test/java/com/workflow/github/IntegrationEndpointTest.java
@@ -26,6 +26,6 @@ public class IntegrationEndpointTest {
 
         // ASSERT
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).isEqualTo("Hello 5");
+        assertThat(response.getBody()).isEqualTo("Hello 6");
     }
 }
