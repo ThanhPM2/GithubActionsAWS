d6a8544bdb516c36d311f775f8343b8e66e0c955
ThanhPM <>
2022-08-31 15:03:11 +0700

    rerun the pipeline

--- src/main/java/com/workflow/github/WorkflowApplication.java
+++ src/main/java/com/workflow/github/WorkflowApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WorkflowApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WorkflowApplication.class, args);
     }
